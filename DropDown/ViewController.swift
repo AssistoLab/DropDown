8faa8ec1107a2536e14a50f01141b2ad1ffac6ca
kevin-hirsch <kevin.hirsch.be@gmail.com>
2015-07-28 17:03:08 +0200

    Update demo

--- DropDown/ViewController.swift
+++ DropDown/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var dropDown: DropDown!
+	
+	private var hidden = true
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		dropDown.datasource = [
+			"Car",
+			"Motorcycle",
+			"Van",
+			"Truck",
+			"Bus"
+		]
+		
+		dropDown.selectionAction = { (string, index) in
+			println("item \(string) at index \(index) selected")
+		}
 	}
-
-
+	
+	@IBAction func showOrDismiss(sender: AnyObject) {
+		if hidden {
+			dropDown.show()
+		} else {
+			dropDown.hide()
+		}
+		
+		hidden = !hidden
+	}
 }
 
