a1f6856f1bd1e36e0e0c7e99facacd3e89fc7233
Kevin Hirsch <kevin@pinch.eu>
2016-06-13 16:04:41 +0200

    Restructure project into demo and framework

--- Demo/AppDelegate.swift
+++ Demo/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DropDown
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import DropDown
 
 class ViewController: UIViewController {
 
