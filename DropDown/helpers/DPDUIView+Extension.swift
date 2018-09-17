a9698236ba6e79625f708a38b1465fbe89087d51
Jiří Chlum <chlum.jiri@gmail.com>
2018-09-17 17:10:32 +0200

    fix window Level

--- DropDown/helpers/DPDUIView+Extension.swift
+++ DropDown/helpers/DPDUIView+Extension.swift
@@ -44,7 +44,7 @@ internal extension UIWindow {
 			let frontToBackWindows = Array(UIApplication.shared.windows.reversed()) 
 			
 			for window in frontToBackWindows {
-				if window.windowLevel == UIWindowLevelNormal {
+				if window.windowLevel == UIWindow.Level.normal {
 					currentWindow = window
 					break
 				}
