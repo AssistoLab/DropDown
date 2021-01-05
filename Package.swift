d040a7d331a978d0e6b6568a40bb017aac127720
Tysiachnik, Roman <roman.tysiachnik@sigma.software>
2021-01-05 16:15:42 +0200

    Fixed no xib crash when DropDown was added using SPM and updated swift-tools

--- Package.swift
+++ Package.swift
@@ -1,20 +1,27 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "DropDown",
     platforms: [
-        .iOS(.v8)
+        .iOS(.v9)
     ],
     products: [
         .library(
             name: "DropDown",
-            targets: ["DropDown"])
+            targets: ["DropDown"]
+        )
     ],
     targets: [
         .target(
             name: "DropDown",
-            path: "DropDown"
+            dependencies: [],
+            path: "DropDown",
+            exclude: ["Info.plist", "DropDown.h"],
+            resources: [
+              .process("DropDown/resources")
+            ]
         )
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
